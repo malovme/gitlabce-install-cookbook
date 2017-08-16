e7e6525c5f4e9bd08f1b4170759b8bb56883934a
Michael Malov <mmeemail@gmail.com>
2017-08-16 18:15:09 +0300

    dont run gitlabshell on db instance

--- cookbooks/gitlabshell-custom/recipes/default.rb
+++ cookbooks/gitlabshell-custom/recipes/default.rb
@@ -4,16 +4,18 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-current_path = "/data/gitlabce/current"
-failed_path = "/data/gitlabce/releases_failed"
+if %w[util app app_master].include?(node['dna']['instance_role'])
+  current_path = "/data/gitlabce/current"
+  failed_path = "/data/gitlabce/releases_failed"
 
-if File.exist?(current_path)
-  release_dir = current_path
-else
-  release_dir = failed_path + '/' +
-      Dir.entries(failed_path).select {|entry| File.directory? File.join(failed_path, entry) and !(entry =='.' || entry == '..') }.sort.last
-end
+  if File.exist?(current_path)
+    release_dir = current_path
+  else
+    release_dir = failed_path + '/' +
+        Dir.entries(failed_path).select {|entry| File.directory? File.join(failed_path, entry) and !(entry =='.' || entry == '..') }.sort.last
+  end
 
-execute 'bundle exec rake gitlab:shell:install REDIS_URL=unix:/var/run/redis/redis.sock RAILS_ENV=production SKIP_STORAGE_VALIDATION=true' do
-  cwd release_dir
+  execute 'bundle exec rake gitlab:shell:install REDIS_URL=unix:/var/run/redis/redis.sock RAILS_ENV=production SKIP_STORAGE_VALIDATION=true' do
+    cwd release_dir
+  end
 end
