1ab1682799801a45e3c4d28815ac66b45903eef8
Michael Malov <mmeemail@gmail.com>
2017-08-16 20:00:53 +0300

    fix of pg extension creating

--- cookbooks/gitlabdb-custom/recipes/default.rb
+++ cookbooks/gitlabdb-custom/recipes/default.rb
@@ -5,8 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 if %w[db_master db_slave].include?(node['dna']['instance_role'])
-  require "pg"
-
-  c = PGconn.connect(:user=>'postgres')
-  c.exec("CREATE EXTENSION pg_trgm;")
+  execute 'psql command' do
+    command 'sudo su postgres && psql -c "CREATE EXTENSION pg_trgm;"'
+  end
 end
