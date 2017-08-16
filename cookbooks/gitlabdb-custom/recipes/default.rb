a3b1dbec041a09e5603fb75cb50013207c984b5a
Michael Malov <mmeemail@gmail.com>
2017-08-17 00:26:51 +0300

    pg extension add IF NOT EXISTS parameter

--- cookbooks/gitlabdb-custom/recipes/default.rb
+++ cookbooks/gitlabdb-custom/recipes/default.rb
@@ -6,6 +6,6 @@
 
 if %w[db_master db_slave].include?(node['dna']['instance_role'])
   execute 'psql command' do
-    command 'sudo su postgres && psql -c "CREATE EXTENSION pg_trgm;"'
+    command 'sudo su postgres && psql -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"'
   end
 end
