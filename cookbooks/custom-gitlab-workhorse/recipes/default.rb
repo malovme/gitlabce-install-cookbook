3d32b2be66dab8db88f645568b69cd5aa1aa1dc0
Michael Malov <mmeemail@gmail.com>
2017-08-18 16:39:36 +0300

    delete workhorse before install

--- cookbooks/custom-gitlab-workhorse/recipes/default.rb
+++ cookbooks/custom-gitlab-workhorse/recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 if %w[util app app_master].include?(node['dna']['instance_role'])
+  directory '/home/git/gitlab-workhorse' do
+    action :delete
+  end
+
   execute 'gitlab-workhorse step_1' do
     cwd '/home/git'
     command 'sudo -u deploy -H git clone https://gitlab.com/gitlab-org/gitlab-workhorse.git'
