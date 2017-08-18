0fdb66bd27864b1b1c9f8e5ed3ecb23f7936a913
Michael Malov <mmeemail@gmail.com>
2017-08-18 15:25:59 +0300

    gitlab-workhorse do not install on db instances

--- cookbooks/custom-gitlab-workhorse/recipes/default.rb
+++ cookbooks/custom-gitlab-workhorse/recipes/default.rb
@@ -3,7 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-execute 'sudo -u deploy -H bundle exec rake "gitlab:workhorse:install[/home/git/gitlab-workhorse]" RAILS_ENV=production' do
-  cwd '/data/gitlabce/current'
-  only_if { File.exist?("/data/gitlabce/current") }
+if %w[util app app_master].include?(node['dna']['instance_role'])
+  execute 'sudo -u deploy -H bundle exec rake "gitlab:workhorse:install[/home/git/gitlab-workhorse]" RAILS_ENV=production' do
+    cwd '/data/gitlabce/current'
+    only_if { File.exist?("/data/gitlabce/current") }
+  end
 end
