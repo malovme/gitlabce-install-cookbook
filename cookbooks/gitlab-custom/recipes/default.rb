d35a73cd9ab6d015cdabd971d7a3b152c3af84c4
Michael Malov <mmeemail@gmail.com>
2017-08-18 16:27:27 +0300

    create uploads dir

--- cookbooks/gitlab-custom/recipes/default.rb
+++ cookbooks/gitlab-custom/recipes/default.rb
@@ -4,6 +4,15 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if File.exists?('/data/gitlabce/current') && !File.exists?('/data/gitlabce/current/public/uploads')
+  directory '/data/gitlabce/current/public/uploads' do
+    owner 'deploy'
+    group 'deploy'
+    mode '0755'
+    action :create
+  end
+end
+
 directory '/home/git' do
   owner 'deploy'
   group 'deploy'
