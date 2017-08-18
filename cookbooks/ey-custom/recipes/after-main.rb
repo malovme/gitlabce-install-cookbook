5c2e48dd14e39147f3e97a28fdf1f7b8bca227f5
Michael Malov <mmeemail@gmail.com>
2017-08-18 16:37:50 +0300

    change order of recipes

--- cookbooks/ey-custom/recipes/after-main.rb
+++ cookbooks/ey-custom/recipes/after-main.rb
@@ -1,7 +1,7 @@
 include_recipe "custom-redis"
+include_recipe 'custom-go'
 include_recipe 'gitlab-custom'
 include_recipe 'gitlabdb-custom'
 #include_recipe 'envyable-custom'
 include_recipe 'gitlabshell-custom'
 include_recipe 'custom-gitlab-workhorse'
-include_recipe 'custom-go'
